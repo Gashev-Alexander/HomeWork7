011062d8783d5dd32253024d3dfd1551d140a791
Gashev-Alexander <gashev91@gmail.com>
2022-12-28 00:46:35 +0300

    Доделать код по удалению контакта

--- controller.py
+++ controller.py
@@ -24,7 +24,7 @@ def choice_todo():
     1 - Создать контакт.\n\
     2 - Показать все контакты. \n\
     3 - Поиск и изменение контакта. \n\
-    4 - Выход")
+    4 - Поиск и удаление контакта. ")
     ch = input("Введите цифру: ")
     if ch == '1':
         sep = choice_sep()
@@ -42,14 +42,12 @@ def choice_todo():
             print(item[0].center(20), item[1].center(25), item[2].center(30), item[3].center(30))
             def LineReplacement(File, FindThis, ReplaceByThis):
                 TemporaryFile = File + '.tmp'
-                result = 0
                 with open(File, 'r') as f1, open(TemporaryFile, 'w') as f2:
                     lines = f1.readlines()
                     for line in lines:
                         line = line.strip()
                         if line == FindThis:
                             f2.write(ReplaceByThis + '\n')
-                            result = result + 1
                         else:
                             f2.write(line + '\n') 
                         path = os.path.join(os.path.abspath(os.path.dirname(__file__)), File)
@@ -62,4 +60,35 @@ def choice_todo():
         else:
             print("Данные не обнаружены")
     elif ch == '4':
-       exit
+        word = input("Введите данные для поиска: ")
+        data = export_data()
+        item = search_data(word, data)
+        if item != None:
+            print("Имя".center(20), "Фамилия".center(25), "Номер Телефона".center(30), "Комментарий".center(30))
+            print("-"*100)
+            print(item[0].center(20), item[1].center(25), item[2].center(30), item[3].center(30))
+            print('Для Удаления введите 1.\n\
+Для выхода введите 2.')
+            f_del = input('Ожидание ввода: ')
+            if f_del == '1':
+                def Delete_line(File, DelThis, ReplaceDelThis):
+                    TemporaryFile = File + '.tmp'
+                    with open(File, 'r') as f1, open(TemporaryFile, 'w') as f2:
+                        lines = f1.readlines()
+                        for line in lines:
+                            line = line.strip()
+                            if line == DelThis:
+                                f2.write(ReplaceDelThis + '\n')
+                            else:
+                                f2.write(line + '\n') 
+                            path = os.path.join(os.path.abspath(os.path.dirname(__file__)), File)
+                    os.remove(path)
+                    os.rename('phone.txt.tmp', 'phone.txt')
+                File = "phone.txt"
+                DelThis = (item[0], item[1], item[2], item[3])
+                ReplaceDelThis = exit #Здесь будет код на удаление строки в фйле
+                Delete_line(File, DelThis, ReplaceDelThis)
+            elif f_del == '2':
+                    exit
+        else:
+            exit
